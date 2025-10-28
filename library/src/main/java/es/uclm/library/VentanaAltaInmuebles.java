9daef10444516888255b5e5eafce2e668cf6a0f4
MiguelAhijon <149965218+MiguelAhijon@users.noreply.github.com>
2025-10-28 19:17:47 +0100

    Implement property management controller methods

--- library/src/main/java/es/uclm/library/VentanaAltaInmuebles.java
+++ library/src/main/java/es/uclm/library/VentanaAltaInmuebles.java
@@ -1,4 +1,34 @@
 package es.uclm.library;
 
+import es.uclm.library.business.controller.GestorInmuebles;
+import es.uclm.library.business.entity.Inmueble;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/inmuebles")
 public class VentanaAltaInmuebles {
+
+    @Autowired
+    private GestorInmuebles gestorInmuebles;
+
+    @GetMapping("/listar")
+    public String listarInmuebles(Model model) {
+        model.addAttribute("inmuebles", gestorInmuebles.listarInmuebles());
+        return "inmuebles";
+    }
+
+    @GetMapping("/nuevo")
+    public String mostrarFormularioAlta(Model model) {
+        model.addAttribute("inmueble", new Inmueble());
+        return "inmueble_nuevo";
+    }
+
+    @PostMapping("/guardar")
+    public String registrarInmueble(@ModelAttribute Inmueble inmueble, Model model) {
+        gestorInmuebles.registrarInmueble(inmueble);
+        return "redirect:/inmuebles/listar";
+    }
 }
