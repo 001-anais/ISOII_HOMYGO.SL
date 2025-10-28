9abc6cbf505e981b30c7c82a0e3be8e32b615350
davidmarcos2 <149959886+davidmarcos2@users.noreply.github.com>
2025-10-28 19:24:30 +0100

    Update VentanaBusqueda.java

--- library/src/main/java/es/uclm/library/VentanaBusqueda.java
+++ library/src/main/java/es/uclm/library/VentanaBusqueda.java
@@ -1,4 +1,33 @@
 package es.uclm.library;
 
+import es.uclm.library.business.controller.GestorBusquedas;
+import es.uclm.library.business.entity.Inmueble;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/busqueda")
 public class VentanaBusqueda {
+
+    @Autowired
+    private GestorBusquedas gestorBusquedas;
+
+    @GetMapping
+    public String mostrarFormularioBusqueda() {
+        return "busqueda";
+    }
+
+    @PostMapping
+    public String buscarInmuebles(@RequestParam String direccion,
+                                  @RequestParam double precioMaximo,
+                                  Model model) {
+        List<Inmueble> resultados = gestorBusquedas.buscarPorDireccion(direccion);
+        resultados.retainAll(gestorBusquedas.buscarPorPrecioMaximo(precioMaximo));
+        model.addAttribute("resultados", resultados);
+        return "resultados";
+    }
 }
