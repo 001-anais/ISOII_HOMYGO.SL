615a02a28c2ded489060da8d78fa1fa5c8d0e031
MiguelAhijon <149965218+MiguelAhijon@users.noreply.github.com>
2025-10-28 19:20:30 +0100

    Refactor AbstractEntityDAO to implement optional methods

--- library/src/main/java/es/uclm/library/persistence/AbstractEntityDAO.java
+++ library/src/main/java/es/uclm/library/persistence/AbstractEntityDAO.java
@@ -1,27 +1,24 @@
 package es.uclm.library.persistence;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * DAO base genérico. Proporciona acceso al EntityManager y operaciones
+ * por defecto (opcionales) para quienes extiendan esta clase.
+ */
 public abstract class AbstractEntityDAO {
 
-	GestorBD gestorBD;
+    protected final Logger log = LoggerFactory.getLogger(getClass());
 
-	public void selectEntity() {
-		// TODO - implement AbstractEntityDAO.selectEntity
-		throw new UnsupportedOperationException();
-	}
+    @PersistenceContext
+    protected EntityManager em;
 
-	public void updateEntity() {
-		// TODO - implement AbstractEntityDAO.updateEntity
-		throw new UnsupportedOperationException();
-	}
-
-	public void saveEntity() {
-		// TODO - implement AbstractEntityDAO.saveEntity
-		throw new UnsupportedOperationException();
-	}
-
-	public void deleteEntity() {
-		// TODO - implement AbstractEntityDAO.deleteEntity
-		throw new UnsupportedOperationException();
-	}
-
+    /** Operaciones genéricas opcionales (no usadas por repos Spring Data) */
+    public void selectEntity() { /* opcional */ }
+    public void updateEntity() { /* opcional */ }
+    public void saveEntity()   { /* opcional */ }
+    public void deleteEntity() { /* opcional */ }
 }
