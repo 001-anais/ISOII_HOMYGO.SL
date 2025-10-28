aa8975fea764deb52ea63ebbe324e486737ff4db
davidmarcos2 <149959886+davidmarcos2@users.noreply.github.com>
2025-10-28 19:27:19 +0100

    Update VentanaReservas.java

--- library/src/main/java/es/uclm/library/VentanaReservas.java
+++ library/src/main/java/es/uclm/library/VentanaReservas.java
@@ -1,5 +1,34 @@
 package es.uclm.library;
 
+import es.uclm.library.business.controller.GestorReservas;
+import es.uclm.library.business.entity.Reserva;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/reservas")
 public class VentanaReservas {
-	
+
+    @Autowired
+    private GestorReservas gestorReservas;
+
+    @GetMapping("/listar")
+    public String listarReservas(Model model) {
+        model.addAttribute("reservas", gestorReservas.listarReservas());
+        return "reservas";
+    }
+
+    @GetMapping("/nueva")
+    public String mostrarFormularioReserva(Model model) {
+        model.addAttribute("reserva", new Reserva());
+        return "reserva_nueva";
+    }
+
+    @PostMapping("/guardar")
+    public String registrarReserva(@ModelAttribute Reserva reserva) {
+        gestorReservas.registrarReserva(reserva);
+        return "redirect:/reservas/listar";
+    }
 }
