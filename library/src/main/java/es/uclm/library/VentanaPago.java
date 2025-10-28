519c3950e937d7c8dda7a8dd6b2f3adde7dde4ea
davidmarcos2 <149959886+davidmarcos2@users.noreply.github.com>
2025-10-28 19:25:06 +0100

    Update VentanaPago.java

--- library/src/main/java/es/uclm/library/VentanaPago.java
+++ library/src/main/java/es/uclm/library/VentanaPago.java
@@ -1,5 +1,29 @@
 package es.uclm.library;
 
+import es.uclm.library.business.controller.GestorPagos;
+import es.uclm.library.business.entity.Pago;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/pago")
 public class VentanaPago {
-	
+
+    @Autowired
+    private GestorPagos gestorPagos;
+
+    @GetMapping("/nuevo")
+    public String mostrarFormularioPago(Model model) {
+        model.addAttribute("pago", new Pago());
+        return "pago";
+    }
+
+    @PostMapping("/procesar")
+    public String procesarPago(@ModelAttribute Pago pago, Model model) {
+        gestorPagos.registrarPago(pago);
+        model.addAttribute("mensaje", "Pago realizado correctamente.");
+        return "resultado_pago";
+    }
 }
