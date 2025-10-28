eab16ca14fdbcb976932074a7fc490a93d98f272
001-anais <Anais.Chiquito@alu.uclm.es>
2025-10-28 19:30:38 +0100

    GestorReservas Actualizado

--- library/src/main/java/es/uclm/library/business/controller/GestorReservas.java
+++ library/src/main/java/es/uclm/library/business/controller/GestorReservas.java
@@ -1,10 +1,31 @@
 package es.uclm.library.business.controller;
 
-import es.uclm.library.persistence.ReservaDAO;
-import es.uclm.library.persistence.*;
+import es.uclm.library.business.entity.Reserva;
+import es.uclm.library.business.repository.ReservaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
+@Service
 public class GestorReservas {
 
-	ReservaDAO reservaDAO;
+    @Autowired
+    private ReservaRepository reservaRepository;
 
+    public Reserva registrarReserva(Reserva reserva) {
+        return reservaRepository.save(reserva);
+    }
+
+    public List<Reserva> listarReservas() {
+        return reservaRepository.findAll();
+    }
+
+    public Optional<Reserva> buscarPorId(int id) {
+        return reservaRepository.findById(id);
+    }
+
+    public void eliminarReserva(int id) {
+        reservaRepository.deleteById(id);
+    }
 }
