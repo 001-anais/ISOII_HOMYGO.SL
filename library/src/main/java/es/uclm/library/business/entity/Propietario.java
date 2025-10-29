abb7477c5114063280da9c33a385e073aac76b9f
MiguelAhijon <149965218+MiguelAhijon@users.noreply.github.com>
2025-10-29 17:19:56 +0100

    Add properties field and methods to Propietario class

--- library/src/main/java/es/uclm/library/business/entity/Propietario.java
+++ library/src/main/java/es/uclm/library/business/entity/Propietario.java
@@ -1,12 +1,22 @@
 package es.uclm.library.business.entity;
 
 import jakarta.persistence.*;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
+@Entity
+@Table(name = "propietarios")
 public class Propietario extends Usuario {
 
-	Collection<Inmueble> propiedades;
+    @OneToMany(mappedBy = "propietario", cascade = CascadeType.ALL)
+    private List<Inmueble> propiedades = new ArrayList<>();
 
+    public Propietario() {}
+
+    public List<Inmueble> getPropiedades() {
+        return propiedades;
+    }
+
+    public void setPropiedades(List<Inmueble> propiedades) {
+        this.propiedades = propiedades;
+    }
 }
