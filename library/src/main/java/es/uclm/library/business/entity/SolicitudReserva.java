f2da01424e0c3cea483a64f7f6f1a966c3bfce9c
MiguelAhijon <149965218+MiguelAhijon@users.noreply.github.com>
2025-10-29 17:21:11 +0100

    Refactor SolicitudReserva class with JPA annotations

--- library/src/main/java/es/uclm/library/business/entity/SolicitudReserva.java
+++ library/src/main/java/es/uclm/library/business/entity/SolicitudReserva.java
@@ -2,15 +2,34 @@ package es.uclm.library.business.entity;
 
 import jakarta.persistence.*;
 
-public class SolicitudReserva extends Reserva {
+@Entity
+@Table(name = "solicitudes_reserva")
+public class SolicitudReserva {
 
-	Inmueble inmueble;
-	Reserva reservaConfirmada;
-	private boolean confirmada;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
-	public void confirmarReserva() {
-		// TODO - implement SolicitudReserva.confirmarReserva
-		throw new UnsupportedOperationException();
-	}
+    @ManyToOne
+    @JoinColumn(name = "inmueble_id")
+    private Inmueble inmueble;
 
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "reserva_confirmada_id")
+    private Reserva reservaConfirmada;
+
+    private boolean confirmada;
+
+    public SolicitudReserva() {}
+
+    public int getId() { return id; }
+
+    public Inmueble getInmueble() { return inmueble; }
+    public void setInmueble(Inmueble inmueble) { this.inmueble = inmueble; }
+
+    public Reserva getReservaConfirmada() { return reservaConfirmada; }
+    public void setReservaConfirmada(Reserva reservaConfirmada) { this.reservaConfirmada = reservaConfirmada; }
+
+    public boolean isConfirmada() { return confirmada; }
+    public void setConfirmada(boolean confirmada) { this.confirmada = confirmada; }
 }
