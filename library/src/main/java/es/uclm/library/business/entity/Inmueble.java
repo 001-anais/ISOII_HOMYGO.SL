0dc3245d57bc9833c2507926e8ecea1ae8bfc715
MiguelAhijon <149965218+MiguelAhijon@users.noreply.github.com>
2025-10-29 17:17:04 +0100

    Define Inmueble
    Added entity annotations and relationships for Inmueble class.

--- library/src/main/java/es/uclm/library/business/entity/Inmueble.java
+++ library/src/main/java/es/uclm/library/business/entity/Inmueble.java
@@ -1,18 +1,45 @@
 package es.uclm.library.business.entity;
 
 import jakarta.persistence.*;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
+@Entity
+@Table(name = "inmuebles")
 public class Inmueble {
 
-	Propietario propietario;
-	Collection<Reserva> reservas;
-	Collection<ListaDeseos> listaDeseos;
-	Collection<Disponibilidad> disponibilidades;
-	Collection<SolicitudReserva> solicitudesReserva;
-	private String direccion;
-	private double precioNoche;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
+    @ManyToOne
+    @JoinColumn(name = "propietario_id")
+    private Propietario propietario;
+
+    @OneToMany(mappedBy = "inmueble", cascade = CascadeType.ALL)
+    private List<Reserva> reservas = new ArrayList<>();
+
+    @ManyToMany(mappedBy = "inmuebles", cascade = CascadeType.ALL)
+    private List<ListaDeseos> listaDeseos = new ArrayList<>();
+
+    private String direccion;
+    private double precioNoche;
+
+    public Inmueble() {}
+
+    public int getId() { return id; }
+
+    public Propietario getPropietario() { return propietario; }
+    public void setPropietario(Propietario propietario) { this.propietario = propietario; }
+
+    public List<Reserva> getReservas() { return reservas; }
+    public void setReservas(List<Reserva> reservas) { this.reservas = reservas; }
+
+    public String getDireccion() { return direccion; }
+    public void setDireccion(String direccion) { this.direccion = direccion; }
+
+    public double getPrecioNoche() { return precioNoche; }
+    public void setPrecioNoche(double precioNoche) { this.precioNoche = precioNoche; }
+
+    public List<ListaDeseos> getListaDeseos() { return listaDeseos; }
+    public void setListaDeseos(List<ListaDeseos> listaDeseos) { this.listaDeseos = listaDeseos; }
 }
