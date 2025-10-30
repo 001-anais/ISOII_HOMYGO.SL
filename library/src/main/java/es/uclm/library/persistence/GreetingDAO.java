24f25f478aa5b7b47105ba863ee4f9c605097cd4
MiguelAhijon <149965218+MiguelAhijon@users.noreply.github.com>
2025-10-30 18:42:30 +0100

    Remove duplicate import statement in GreetingDAO

--- library/src/main/java/es/uclm/library/persistence/GreetingDAO.java
+++ library/src/main/java/es/uclm/library/persistence/GreetingDAO.java
@@ -1,10 +1,10 @@
 package es.uclm.library.persistence;
 
+import es.uclm.library.business.entity.Greeting;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import es.uclm.library.business.entity.Greeting;
 
 @Repository
 public interface GreetingDAO extends JpaRepository<Greeting, Long> {
-
+    // Aquí podrías añadir métodos de consulta específicos si los necesitas.
 }
