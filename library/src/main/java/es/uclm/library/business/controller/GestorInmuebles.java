d1e4df02052ca246ec076e849ed3351f466c8dc5
001-anais <Anais.Chiquito@alu.uclm.es>
2025-10-30 18:32:56 +0100

    GestorInmuebles Actualizado

--- library/src/main/java/es/uclm/library/business/controller/GestorInmuebles.java
+++ library/src/main/java/es/uclm/library/business/controller/GestorInmuebles.java
@@ -5,18 +5,30 @@ import es.uclm.library.business.repository.InmuebleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
-public class GestorBusquedas {
+public class GestorInmuebles {
 
     @Autowired
     private InmuebleRepository inmuebleRepository;
 
-    public List<Inmueble> buscarPorDireccion(String direccion) {
-        return inmuebleRepository.findByDireccionContainingIgnoreCase(direccion);
+    public List<Inmueble> listarInmuebles() {
+        return inmuebleRepository.findAll();
     }
 
-    public List<Inmueble> buscarPorPrecioMaximo(double precioMaximo) {
-        return inmuebleRepository.findByPrecioNocheLessThanEqual(precioMaximo);
+    public Inmueble registrarInmueble(Inmueble inmueble) {
+    	 if (inmueble.getImagen() == null || inmueble.getImagen().isEmpty()) {
+    	        inmueble.setImagen("/img/default_house.jpg");
+    	    }
+    	return inmuebleRepository.save(inmueble);
+    }
+
+    public Optional<Inmueble> buscarPorId(int id) {
+        return inmuebleRepository.findById(id);
+    }
+
+    public void eliminarInmueble(int id) {
+        inmuebleRepository.deleteById(id);
     }
 }
