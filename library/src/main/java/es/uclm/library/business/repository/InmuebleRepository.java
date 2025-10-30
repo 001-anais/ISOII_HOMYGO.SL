c7c7ee299ee2078ec3fbaba83264ed97da0e9a2f
MiguelAhijon <149965218+MiguelAhijon@users.noreply.github.com>
2025-10-30 18:44:01 +0100

    Add Repository annotation to InmuebleRepository

--- library/src/main/java/es/uclm/library/business/repository/InmuebleRepository.java
+++ library/src/main/java/es/uclm/library/business/repository/InmuebleRepository.java
@@ -3,6 +3,7 @@ package es.uclm.library.business.repository;
 import es.uclm.library.business.entity.Inmueble;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import org.springframework.stereotype.Repository;
 public interface InmuebleRepository extends JpaRepository<Inmueble, Integer> {
     List<Inmueble> findByDireccionContainingIgnoreCase(String direccion);
     List<Inmueble> findByPrecioNocheLessThanEqual(double precio);
